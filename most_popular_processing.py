1eec7195eff9d87be01769cfdb3051e865acda05
kaushik16 <kaushik.r.16@gmail.com>
2018-04-22 13:43:19 -0500

    Identify the most popular songs for the entire dataset and for every artist

---
+++ most_popular_processing.py
@@ -0,0 +1,57 @@
+import json
+import pickle
+import math
+import heapq
+from collections import defaultdict
+
+def save_obj(obj, name ):
+    with open('obj/'+ name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+song_playlist_count, song_follower_count = defaultdict(int), defaultdict(int)
+max_playlist_count, max_follower_count = 0, 0
+song_to_artist = {}
+
+song_count, start = 0, 0
+songsData = {}
+
+for i in range(1000):
+    end = start + 999
+    with open('data/mpd.slice.'+str(start)+'-'+str(end)+'.json') as f:
+        slice = json.load(f)
+
+    for playlist in slice['playlists']:
+        for song in playlist['tracks']:
+            if song['track_uri'] not in songsData:
+                songsData[song['track_uri']] = song_count
+                song_count += 1
+            index = songsData[song['track_uri']]
+            song_playlist_count[index] += 1
+            song_follower_count[index] += playlist['num_followers']
+            max_follower_count, max_playlist_count = max(max_follower_count, song_follower_count[index]), max(max_playlist_count, song_playlist_count[index])
+            song_to_artist[index] = song['artist_name']
+    start = end + 1
+    print("Slice : " + str(i) + " - Parsed")
+
+max_playlist_count, max_follower_count = math.log10(max_playlist_count), math.log10(max_follower_count)
+song_score = {song: (0.5*math.log10(song_follower_count[song])/max_follower_count) + (0.5*math.log10(song_playlist_count[song])/max_playlist_count) for song in song_playlist_count}
+
+most_popular_songs = []
+most_popular_by_artist = defaultdict(list)
+for i in range(1000):
+    heapq.heappush(most_popular_songs, (0,''))
+for k, v in song_score.items():
+    if v > most_popular_songs[0][0]: heapq.heappushpop(most_popular_songs, (v,k))
+
+    elem = most_popular_by_artist[song_to_artist[k]]
+    if len(elem) >= 100 and elem[0][0] < v: heapq.heappushpop(elem, (v, k))
+    else: heapq.heappush(elem, (v, k))
+
+most_popular_songs.sort(key = lambda x: x[0], reverse=True)
+for k,v in most_popular_by_artist.items():
+    v.sort(key = lambda x: x[0], reverse = True)
+
+print len(most_popular_by_artist)
+print "Processed"
+save_obj(most_popular_songs, "MostPopularSongs_1000")
+save_obj(most_popular_by_artist, "MostPopularSongsByArtist_100")
